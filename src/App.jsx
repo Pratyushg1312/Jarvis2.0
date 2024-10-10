94f7a8606cee7cb4ae4287b5f08eb908393a16ae
anmol <anmol.gour143@gmail.com>
2024-10-10 15:02:08 +0530

    New UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import SideBar from "./Components/CommonComponent/SideBar/SideBar.jsx";
 import TopBar from "./Components/CommonComponent/TopBar/TopBar.jsx";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Button from "@mui/material/Button";
+import { AppleLogo } from "@phosphor-icons/react";
 
 function App() {
   const checkbox = useRef(null);
@@ -38,26 +44,26 @@ function App() {
           id="toggle-sidebar"
           className="toggle-sidebar-checkbox"
         />
-        <SideBar />
-        <div className="pageWrapper">
-          <TopBar />
+        <TopBar />
+        <div className="pageWrapper" s>
+          <SideBar />
           <div className="pageBody">
             <div className="card">
               <div className="card-header">
-                <div className="cardTitle">
+                <div className="cardHeading">
                   <h4>Input</h4>
                 </div>
               </div>
               <div className="card-body">
                 <div className="row">
-                  <div className="col-md-4 mb20">
+                  <div className="col-md-4">
                     <TextField
                       id="outlined-basic"
                       label="Outlined"
                       variant="outlined"
                     />
                   </div>
-                  <div className="col-md-4 mb20">
+                  <div className="col-md-4">
                     <Autocomplete
                       disablePortal
                       options={["Hello", "Anmol"]}
@@ -66,28 +72,30 @@ function App() {
                       )}
                     />
                   </div>
-                  <div className="col-md-4 mb20">
+                  <div className="col-md-4">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DemoContainer components={["DatePicker"]}>
+                        <DatePicker label="Basic date picker" />
+                      </DemoContainer>
+                    </LocalizationProvider>
+                  </div>
+                  <div className="col-md-4">
+                    <Button color="info" variant="contained">
+                      Contained
+                    </Button>
+                    &nbsp;
+                    <Button color="info" variant="outlined">
+                      Contained
+                    </Button>
+                  </div>
+                  <div className="col-md-4">
                     <TextField
                       id="outlined-basic"
                       label="Outlined"
                       variant="outlined"
                     />
                   </div>
-                  <div className="col-md-4 mb20">
-                    <TextField
-                      id="outlined-basic"
-                      label="Outlined"
-                      variant="outlined"
-                    />
-                  </div>
-                  <div className="col-md-4 mb20">
-                    <TextField
-                      id="outlined-basic"
-                      label="Outlined"
-                      variant="outlined"
-                    />
-                  </div>
-                  <div className="col-md-4 mb20">
+                  <div className="col-md-4">
                     <TextField
                       id="outlined-basic"
                       label="Outlined"
--- src/Components/CommonComponent/TopBar/TopBar.jsx
+++ src/Components/CommonComponent/TopBar/TopBar.jsx
@@ -5,12 +5,13 @@ import { GearSix, MagnifyingGlass, SignOut, User } from "@phosphor-icons/react";
 const TopBar = () => {
   return (
     <div className="topBar">
-      <div className="sidebarToggle">
-        <label htmlFor="toggle-sidebar" className="toggle-sidebar-label">
-          <i></i>
-          <i></i>
-          <i></i>
-        </label>
+      <div className="topBarLeft">
+        <div className="brandLogo">
+          <img src="./assets/images/logo/logo.png" width={40} height={40} />
+          <h3>
+            Creative<span>fuel</span>
+          </h3>
+        </div>
       </div>
       <nav className="navbar navbar-expand-sm">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -35,7 +36,9 @@ const TopBar = () => {
                 aria-expanded="false"
               >
                 <Avatar alt="Avatar" src="./assets/images/avatar/anmol.jpeg" />
-                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
+                <h4>
+                  Anmol<span>User</span>
+                </h4>
               </a>
 
               <ul className="dropdown-menu">
